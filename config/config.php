83d7e3a9e66b91b59d6eae961349d0548b9f632a
StefanyLino <stefanylinosilva47@gmail.com>
2025-04-28 10:26:52 -0300

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+// Arquivo de configuração com as constantes do sistema
+
+// Criando variável "ARQUIVO JSON" que envia para outro diretório
+define('ARQUIVO_JSON', __DIR__ . '/../data/veiculos.json');
+define('ARQUIVO_USUARIOS', __DIR__ . '/../data/usuarios.json');
+define('DIARIA_CARRO', 100.00);
+define('DIARIA_MOTO', 50.00);
+
+?>
