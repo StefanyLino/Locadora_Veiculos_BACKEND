77ca71b61a81a8b681b750ce2a4a754290af846e
StefanyLino <stefanylinosilva47@gmail.com>
2025-05-06 11:25:12 -0300

    Config

--- config/config.php
+++ config/config.php
@@ -6,7 +6,6 @@ define('ARQUIVO_JSON', __DIR__ . '/../data/veiculos.json');
 define('ARQUIVO_USUARIOS', __DIR__ . '/../data/usuarios.json');
 define('DIARIA_CARRO', 100.00);
 define('DIARIA_MOTO', 50.00);
-define('BASE_URL', '/public');
 
 
 ?>
